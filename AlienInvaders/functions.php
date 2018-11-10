8b854cd3e3f3c7262dd08a9a68a931e9bd7cc772
ekspensum <ekspensum@interia.pl>
2018-11-10 22:17:30 +0100

    AlienInvaders - php sesja + baza

---
+++ AlienInvaders/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function showUserTable($login, $conn){
+	$userScore = mysqli_query($conn, 'SELECT * FROM `user` INNER JOIN score ON user.idUser = score.idUser WHERE login = "'.$login.'" ORDER BY score desc LIMIT 10') or die("Brak połączenia z bazą danych L6.");
+	echo '<b id="labelTableUser">Wyniki zalogowanego użytkownika:</b>';
+    echo '<table id="tableUser"><tr><th>Lp.</th>    <th>Login</th>    <th>Punkty</th>	<th>Data</th></tr>';
+    $i = 0;
+    while($dataUserScore = mysqli_fetch_assoc($userScore)){
+        $i++;
+        echo '<tr><td>'.$i.'</td> <td>'.$dataUserScore['login'].'</td> <td>'.$dataUserScore['score'].'</td> <td>'.$dataUserScore['dateTime'].'</td></tr>';
+    }
+    echo '</table>';
+}
+
+function showAllUsersTable($conn){
+	$userScore = mysqli_query($conn, 'SELECT MAX(score) AS score, user.login FROM `user` INNER JOIN score ON user.idUser = score.idUser GROUP BY user.login ORDER BY MAX(score) desc LIMIT 10') or die("Brak połączenia z bazą danych L7.");
+	echo '<b id="labelTableAllUsers">Wyniki wszystkich użytkowników:</b>';
+    echo '<table id="tableAllUsers"><tr><th>Lp.</th>    <th>Login</th>    <th>Punkty</th></tr>';
+    $i = 0;
+    while($dataUserScore = mysqli_fetch_assoc($userScore)){
+        $i++;
+        echo '<tr><td>'.$i.'</td> <td>'.$dataUserScore['login'].'</td> <td>'.$dataUserScore['score'].'</td></tr>';
+    }
+    echo '</table>';
+}
+
+function logOut(){
+	ob_start();
+	session_destroy();
+	$result = 0;
+	ob_end_flush();
+}
+?>
--- AlienInvaders/index.php
+++ AlienInvaders/index.php
@@ -1,36 +1,65 @@
 <?php
-    $host="localhost";
-	$user="root";
-	$password=""; 
-	$database="AlienInvaders";
-    $conn=mysqli_connect($host,$user,$password,$database);
+    session_start();
+    require_once 'dbconnect.php';
+    require_once 'functions.php';
+    showAllUsersTable($conn);
     if(isset($_POST['login']) && isset($_POST['password'])){
         if($_POST['login']!="" && $_POST['password']!=""){
-            $queryLogin = 'SELECT login, pssword FROM user WHERE login = "'.$_POST['login'].'"';
-            $result = mysqli_query($conn, $queryLogin) or die("Brak połączenia z bazą danych L1.");
-            $row = mysqli_fetch_assoc($result);
-            if($row['login']==""){
-                $queryInsertUser = 'INSERT INTO user VALUES(null, "'.$_POST['login'].'", "'.$_POST['password'].'")';
-                mysqli_query($conn, $queryInsertUser) or die("Brak połączenia z bazą danych L2.");
-                $last_id = mysqli_insert_id($conn);
-                $queryInsertScore = 'INSERT INTO score VALUE(null,"'.$_POST['totalScore'].'", "'.$last_id.'")';
-                mysqli_query($conn, $queryInsertScore) or die("Brak połączenia z bazą danych L3.");
-            } elseif($row['login']!="" && $row['pssword']==$_POST['password']){
-                $queryAddScore = 'INSERT INTO score VALUES(null, '.$_POST['totalScore'].','.'(SELECT idUser FROM user WHERE login = "'.$_POST['login'].'"))';
-                mysqli_query($conn, $queryAddScore) or die("Brak połączenia z bazą danych L4.");
-            } else
-                echo '<b id="komunikat">Podano nie poprawne hasło lub login.</b>';       
+         
+            if(isset($_POST['addUser'])){
+                    $queryMultiLogin = 'SELECT login FROM user WHERE login = "'.$_POST['login'].'"';
+                    $resulMultiLogin = mysqli_query($conn, $queryMultiLogin) or die("Brak połączenia z bazą danych L1.");
+                    if(mysqli_num_rows($resulMultiLogin) > 0){
+                        echo '<b id="komunikat">Podany login zapisany jest już w bazie danych.<br>Proszę podać inny login.</b>'; 
+                    } else {
+                        $queryInsertUser = 'INSERT INTO user VALUES(null, "'.$_POST['login'].'", "'.$_POST['password'].'")';
+                        mysqli_query($conn, $queryInsertUser) or die("Brak połączenia z bazą danych L2.");
+                        $login = $_POST['login'];
+                        $_SESSION['login'] = $login;
+                        showUserTable($login, $conn);                  
+                    }
+            }
+
+            if(isset($_POST['logIn'])){
+                $queryLogin = 'SELECT login, password FROM user WHERE login = "'.$_POST['login'].'" '.'AND '.'password = "'.$_POST['password'].'"';
+                $result = mysqli_query($conn, $queryLogin) or die("Brak połączenia z bazą danych L3.");
+                $row = mysqli_fetch_assoc($result);
+                if($row['login']!=""){
+                    $login = $_POST['login'];
+                    $_SESSION['login'] = $login;
+                        if($_SESSION['login'] == $row['login'])
+                            showUserTable($login, $conn);
+                } else
+                    echo '<b id="komunikat">Podano nie poprawne hasło lub login<br>bądź brak użytkownika w bazie.</b>';       
+            }
+
+            if(isset($_POST['addScore'])){
+                if($_POST['totalScore'] != ""){
+                    if(isset($_SESSION['login'])) {
+                        $queryLogin = 'SELECT login, password FROM user WHERE login = "'.$_POST['login'].'" '.'AND '.'password = "'.$_POST['password'].'"';
+                        $result = mysqli_query($conn, $queryLogin) or die("Brak połączenia z bazą danych L4.");
+                        $dateTimeNow = new DateTime('NOW');
+                        $row = mysqli_fetch_assoc($result);
+                        if($_SESSION['login'] == $row['login']){
+                            $queryAddScore = 'INSERT INTO score VALUES(null, '.$_POST['totalScore'].', "'.$dateTimeNow->format(DateTime::ISO8601).'",'.'(SELECT idUser FROM user WHERE login = "'.$_POST['login'].'"))';
+                            mysqli_query($conn, $queryAddScore) or die("Brak połączenia z bazą danych L5.");
+                            $login = $_SESSION['login'];
+                            showUserTable($login, $conn);
+                            showAllUsersTable($conn);
+                        }                   
+                    } else
+                        echo '<b id="komunikat">Proszę o zalogowanie się.</b>';                    
+                } else
+                    echo '<b id="komunikat">Brak punktacji.</b>';   
+            }
+
         } else
-            echo '<b id="komunikat">Proszę uzupełnić pola login lub hasło.</b>';
+            echo '<b id="komunikat">Proszę uzupełnić pola login lub hasło.</b>';  
+
     } 
-    $userScore = mysqli_query($conn, "SELECT * FROM `user` INNER JOIN score ON user.idUser = score.idUser ORDER BY score desc LIMIT 10") or die("Brak połączenia z bazą danych L5.");
-    echo '<table><tr><th>Lp.</th>    <th>Login</th>    <th>Punkty</th></tr>';
-    $i = 0;
-    while($dataUserScore = mysqli_fetch_assoc($userScore)){
-        $i++;
-        echo '<tr><td>'.$i.'</td> <td>'.$dataUserScore['login'].'</td> <td>'.$dataUserScore['score'].'</td></tr>';
-    }
-    echo '</table>';
+    
+    if(isset($_POST['logOut'])) logOut();
+    
     mysqli_close($conn);
 ?>
 
@@ -47,8 +76,13 @@
 </head>
 
 <body>
-
+    
     <form action="index.php" method="POST">
+        <label id="labLogin" for="login">Login:</label>
+        <input id="login" name="login" type="text" value="<?php if(isset($_POST['login'])) echo $_POST['login']; ?>" />
+        <label id="labPass" for="pass">Hasło:</label>
+        <input id="pass" name="password" type="password" value="<?php echo $_POST['password']; ?>" />
+        
         <label id="lab1" for="al1">Obcy 1:</label>
         <input id="al1" name="scoreAlien1" type="text" />
         <label id="lab2" for="al2">Obcy 2:</label>
@@ -56,15 +90,15 @@
         <label id="lab3" for="al3">Obcy 3:</label><br/><br/>
         <input id="al3" name="scoreAlien3" type="text" />
         <label id="labTot" for="total">Wynik razem:</label>
-        <input id="total" name="totalScore" type="text" readonly value="<?php if(isset($_POST['totalScore'])) echo $_POST['totalScore']; ?>" />
-        <label id="labLogin" for="login">Login:</label>
-        <input id="login" name="login" type="text" value="<?php if(isset($_POST['login'])) echo $_POST['login']; ?>" />
-        <label id="labPass" for="pass">Hasło:</label>
-        <input id="pass" name="password" type="password" value="<?php echo $_POST['password']; ?>" />
-        <input id="sub" type="submit" value="Dodaj wynik do bazy" />
+        <input id="total" name="totalScore" type="text" readonly value="<?php if(isset($_POST['totalScore'])) {if(isset($result)) $result; else $_POST['totalScore'];} ?>" />
+
+        <input id="buttonLogIn" type="submit" name="logIn" value="Zaloguj" />
+        <input id="buttonAddScore" type="submit" name="addScore" value="Dodaj wynik do bazy" />
+        <input id="buttonAddUser" type="submit" name="addUser" value="Dodaj użytkownika" />
+        <input id="buttonLogOut" type="submit" name="logOut" value="Wyloguj" />
     </form>
 
-    <button id="button" onclick="startGame();">Zacznij grę</button>
+    <button id="buttonStart" onclick="startGame();">Zacznij grę</button>
     <script src="main.js"></script>
 </body>
 
